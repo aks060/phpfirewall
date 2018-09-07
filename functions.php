10026e5a3294f052ade59e0add5049a93f81a2c4
Akash Kumar Sharma <31161908+aks060@users.noreply.github.com>
2018-09-07 16:21:22 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**********************************************************************************************************************************
+                                                STOP IT FUNCTION
+*********************************************************************************************************************************/
+
+function stopit()
+{
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Not Allowed | Protected by Sky</title>
+</head>
+<body style="background-color:black; color:white;">
+	<center><h1>Not Allowed!! Protected by Sky Security</h1></center></body>
+</html>
+<?php
+exit(0);
+}
+
+
+?>
