fae1c37ee026b10bf95e4d168e9618abf6fe1a9a
Akash Kumar Sharma <31161908+aks060@users.noreply.github.com>
2018-09-10 16:03:16 +0530

    Update post_sql.php

--- post_sql.php
+++ post_sql.php
@@ -4,7 +4,26 @@ if($_SERVER['REQUEST_METHOD']=="POST")
 {
   foreach($_POST as $par)
   {
-     $val=$_POST[$par];
-    echo $val;
+    //echo $val;
+    $restrict=array("'", '"', 'order', '*', 'union');
+    
+    /*Comparing each character by char*/
+    for($i=0; $i<strlen($par); $i++)
+    {
+       if($par[$i] in $restrict)
+       {
+           stopit();
+       }
+    }
+    
+    /*Compare words separated by space*/
+    $wd=explode(' ', $par);
+    for($i=0; $i<sizeof($wd); $i++)
+    {
+      if($wd[$i] in $restrict)
+      {
+        stopit();
+      }
+    }
   }
 }
